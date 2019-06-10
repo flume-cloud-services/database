9d619db4d3bbd8f1ab8c1826e9a5affdf2b1876b
Erwan ROUSSEL <erwan.roussel51@gmail.com>
2019-06-10 14:22:47 +0200

    Add authentification
    * Generate go.mod
    
    * Add controllers and routes
    
    * Delete routes folder
    
    * Add 'signin' route and controller
    
    * Add jwt-go
    
    * Add signin route
    
    * Add auth middleware

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/flume-cloud-services/database/controllers"
+	"github.com/flume-cloud-services/database/middleware"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	http.Handle("/welcome", middleware.Middleware(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			io.WriteString(w, "Welcome to you visitor !")
+		}),
+		middleware.AuthMiddleware,
+	))
+	http.HandleFunc("/signin", controllers.Signin)
+
+	log.Println("Starting server on port :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
