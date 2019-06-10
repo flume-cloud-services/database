0ddc1d8f461ee831752ebe4d5a5d747bc8c2cc20
dimensi0n <erwan.roussel51@gmail.com>
2019-06-10 14:19:00 +0200

    Add auth middleware

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 
 	"github.com/flume-cloud-services/database/controllers"
+	"github.com/flume-cloud-services/database/middleware"
 )
 
 func main() {
+	http.Handle("/welcome", middleware.Middleware(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			io.WriteString(w, "Welcome to you visitor !")
+		}),
+		middleware.AuthMiddleware,
+	))
 	http.HandleFunc("/signin", controllers.Signin)
 
 	log.Println("Starting server on port :8080")
